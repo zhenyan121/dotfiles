3be61685ffacf23d5b0fc9d1d43c58a155c6b7cd
zhenyan121 <3367366583@qq.com>
2026-03-25 09:19:31 +0800

    feat: change nvim config to AstroNvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,27 @@
-require("config.lazy")
--- 基础缩进设置
-vim.opt.tabstop = 4      -- 1个Tab显示为4个空格
-vim.opt.shiftwidth = 4   -- 自动缩进时缩进4个空格
-vim.opt.expandtab = true -- 把Tab键变成空格（现代编程通用习惯）
-vim.opt.softtabstop = 4  -- 编辑模式下按退格键退回4个空格
--- 显示行数
-vim.opt.number = true
+-- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
+-- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
+local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
+  -- stylua: ignore
+  local result = vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  if vim.v.shell_error ~= 0 then
+    -- stylua: ignore
+    vim.api.nvim_echo({ { ("Error cloning lazy.nvim:\n%s\n"):format(result), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+    vim.fn.getchar()
+    vim.cmd.quit()
+  end
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- validate that lazy is available
+if not pcall(require, "lazy") then
+  -- stylua: ignore
+  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.fn.getchar()
+  vim.cmd.quit()
+end
+
+require "lazy_setup"
+require "polish"
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,15 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
+
+---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  version = false, -- 建议使用 master 分支
-  build = ":TSUpdate",
-  event = { "BufReadPost", "BufNewFile" }, -- 只有打开文件时才加载，防止启动报错
-  config = function()
-    -- 增加一个保护性检测
-    local status_ok, configs = pcall(require, "nvim-treesitter.configs")
-    if not status_ok then
-      return
-    end
-
-    configs.setup({
-      ensure_installed = { "lua", "vim", "vimdoc", "query", "c", "cpp" }, -- 基础必装
-      highlight = { enable = true },
-      indent = { enable = true }, -- 建议开启，C++ 缩进会更准确
-    })
-  end,
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      -- add more arguments for adding more treesitter parsers
+    },
+  },
 }
